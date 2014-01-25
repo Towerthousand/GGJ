c4d6c2ec112a282266bc571a424203e604332f46
Nico <nico@codrilo.com>
2014-01-26 00:43:09 +0100

    fixed musics

--- SceneMain/SceneMain.cpp
+++ SceneMain/SceneMain.cpp
@@ -75,7 +75,7 @@ SceneMain::~SceneMain() {
 }
 
 void SceneMain::loadResources() {
-    AudioManager::loadMusic("gameMusic", "data/music/GuantepoderosoCut2.ogg");
+    AudioManager::loadMusic("gameMusic", "data/music/Guantepoderoso.ogg");
     AudioManager::getMusic("gameMusic")->setLoop(true);
     AudioManager::getMusic("gameMusic")->play();
 
